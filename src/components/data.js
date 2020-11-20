9d40e5a2fed3229812f1b01dfb6d508e8b6aee5b
Oguzhan <ahmetoguzhany@gmail.com>
2020-11-20 19:02:05 +0300

    commit on 'master'

--- src/components/data.js
+++ src/components/data.js
@@ -13,29 +13,23 @@
         text: "lorem ipsum dolor",
         konular : ['php','html','css']
     },
+    
     {
         id: 3,
-        kaynakismi: "bizimiçinyazılım.com",
-        kategori: "dökümantasyon",
-        text: "lorem ipsum dolor sit amet",
-        konular : ['javascript']
-    },
-    {
-        id: 4,
         kaynakismi: "bizimiçinyazılım2.com",
         kategori: "dökümantasyon",
         text: "lorem ipsum dolor sit amet",
         konular : ['javascript','css']
     },
     {
-        id: 5,
+        id: 4,
         kaynakismi: "yazılımgençliği3.com",
         kategori: "bilgini sına",
         text: "lorem ipsum dolor sit amet",
         konular : ['php','css']
     },
     {
-        id: 6,
+        id: 5,
         kaynakismi: "yazılımgençliği4.com",
         kategori: "bilgini sına",
         text: "lorem ipsum dolor sit",
